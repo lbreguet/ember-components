e25ff5c22106c259a908a726a6950e5dc3a29c71
lbreguet <louis-w@live.com>
2017-03-21 12:14:39 -0400

    Ember components done
    -defined methods to show hide w/o jquery
    -strikethrough list-items with a classnamebinding

--- app/components/listr-list/item.js
+++ app/components/listr-list/item.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNameBindings: ['listItemCompleted'],
+  listItemCompleted: false,
+  actions: {
+    toggleListItem() {
+      return this.toggleProperty('listItemCompleted');
+    }
+  }
 });
